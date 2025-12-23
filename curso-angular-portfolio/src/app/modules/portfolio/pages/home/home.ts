a5ef275656b4f84a8520dc40982d5d943243a49a
Patrickhas <patrickagost@hotmail.com>
2025-12-23 14:22:22 -0300

    seção-10 projeto pratico: portfolio - aula 53

--- curso-angular-portfolio/src/app/modules/portfolio/pages/home/home.ts
+++ curso-angular-portfolio/src/app/modules/portfolio/pages/home/home.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Header } from '../../components/header/header';
 import { Knowledge } from '../../components/knowledge/knowledge';
+import { Experiences } from '../../components/experiences/experiences';
 
 @Component({
   selector: 'app-home',
-  imports: [Header, Knowledge],
+  imports: [Header, Knowledge, Experiences],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
